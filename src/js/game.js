d9cba151e54ecb84ae062bd213b144dbdc7cabfe
FrencisAlreadyInUse <francisdclercq@gmail.com>
2017-12-07 02:17:42 +0100

    started adding physics

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,25 @@
+const $aframeScene = document.querySelector('a-scene');
+const $ball = document.getElementById('bowlingBall');
+
+const handleThrowBall = () => {
+  console.log('[handleThrowBall]', 'clicked');
+};
+
+const handleCollision = (e) => {
+  console.log(`Player has collided with body #${e.detail.body.id}`);
+  console.log(`Player has collided with body #${e.detail.target.el}`);
+  console.log(`Player has collided with body #${e.detail.body.el}`);
+  console.log(`Player has collided with body #${e.detail.contact.ni}`);
+
+  // e.detail.target.el; // Original entity (playerEl).
+  // e.detail.body.el; // Other entity, which playerEl touched.
+  // e.detail.contact; // Stats about the collision (CANNON.ContactEquation).
+  // e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
+};
+
 const init = () => {
-  console.log('game initted');
+  $aframeScene.addEventListener('click', handleThrowBall);
+  $ball.addEventListener('collide', handleCollision);
 };
 
 init();
