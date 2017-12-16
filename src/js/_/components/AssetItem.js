45332131e828dc436363d198b1dce6d6f6357bdc
vgesteljasper <vgesteljasper@gmail.com>
2017-12-17 00:18:27 +0100

    removed old unneeded code

--- src/js/_/components/AssetItem.js
+++
@@ -1,7 +0,0 @@
-import { el } from 'redom';
-
-export default class AssetItem {
-  constructor(attributes) {
-    this.el = el('a-asset-item', attributes);
-  }
-}
--- src/js/_/setAttributes.js
+++
@@ -1,6 +0,0 @@
-export default ($node, attributes) => {
-  // eslint-disable-next-line guard-for-in
-  for (const item in attributes) {
-    $node.setAttribute(item, attributes[item]);
-  }
-};
