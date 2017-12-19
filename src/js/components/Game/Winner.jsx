7729bd4da5bb8e6c8e69f14202f7ae1223813b1b
FrencisAlreadyInUse <francisdclercq@gmail.com>
2017-12-20 00:38:48 +0100

    added Winner Component (close #59)

---
+++ src/js/components/Game/Winner.jsx
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+import { PropTypes } from 'prop-types';
+import { inject, observer } from 'mobx-react';
+
+import Crown from './Crown.jsx';
+
+const Winner = ({ gameStore }) => {
+  console.log('[Winner]', gameStore.currentLeaders);
+
+  return (
+    <Fragment>
+      <Crown position="-0.118 3.908 -7.022" />
+
+      <a-text
+        id="player-winner"
+        position="0.024 3.2 -7"
+        color="#3737DD"
+        wrap-count="15"
+        text="width:6; align:center; value: Japser"
+        font="https://cdn.aframe.io/fonts/KelsonSans.fnt"
+      />
+
+      <a-text
+        id="player-winner--text"
+        position="-0.033 2.556 -7"
+        color="#d1b390"
+        wrap-count="35"
+        text="width:6; align:center; value: Won The Game"
+        font="https://cdn.aframe.io/fonts/KelsonSans.fnt"
+      />
+    </Fragment>
+  );
+};
+
+Winner.propTypes = {
+  gameStore: PropTypes.object.isRequired,
+};
+
+export default inject('gameStore')(observer(Winner));
--- src/js/containers/Game.jsx
+++ src/js/containers/Game.jsx
@@ -6,7 +6,8 @@ import Camera from './../components/Game/Camera.jsx';
 import Assets from './../components/Game/Assets.jsx';
 import Alley from './../components/Game/Alley.jsx';
 import Arrow from './../components/Game/Arrow.jsx';
-import Scores from './../components/Game/Scores.jsx';
+// import Scores from './../components/Game/Scores.jsx';
+import Winner from './../components/Game/Winner.jsx';
 import WhosPlaying from './../components/Game/WhosPlaying.jsx';
 import Ball from './../components/Game/Ball.jsx';
 import BallPeer from './../components/Game/BallPeer.jsx';
@@ -20,7 +21,8 @@ const Game = ({ gameStore }) => (
       <Assets />
       <Alley />
       <Arrow />
-      <Scores />
+      {/* <Scores /> */}
+      <Winner />
       {gameStore.currentPlayingPeer.id !== 'me' ? <WhosPlaying /> : null}
       {gameStore.renderBall ? <Ball /> : null}
       {gameStore.renderPeerBall ? <BallPeer /> : null}
